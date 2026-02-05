136d6b7e4d51d4a9266501d148c6ba4410df9459
ploy <ploy@ploys-MacBook-Air.local>
2026-02-05 12:06:17 +0700

    update about

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://jiraphapa.github.io'),
   title: 'Jiraphapa Jiravaraphan',
   // description: 'Solutions Architect specializing in cloud architecture, system design, and digital transformation.',
   icons: {
